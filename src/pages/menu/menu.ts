c422cce8ece76b0ac718ff895f3689fa3c0e7afe
giampieer <giampieer24@gmail.com>
2018-08-05 00:15:20 -0500

    design list

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,13 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the MenuPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-menu',
@@ -17,17 +9,11 @@ export class MenuPage {
   loadProgress: number = 0;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
   ionViewDidLoad(){
-
     setInterval(() => {
-
-      if(this.loadProgress < 100){
+      if(this.loadProgress < 50){
         this.loadProgress++;
       }
-
     }, 50);
-
   }
-
 }
